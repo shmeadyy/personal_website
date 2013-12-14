375aa91f1b3d3fedc2cb35de914d219a284e64b3
Laura Mead <lauraemead7@gmail.com>
2013-12-13 16:39:58 -0800

    readded the javascript folder, yay

---
+++ public/js/about.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.fade').hover(
+		function(){
+     $(this).find('.caption').fadeIn(250);
+   },
+   function(){
+        $(this).find('.caption').fadeOut(250);
+   });
+});
